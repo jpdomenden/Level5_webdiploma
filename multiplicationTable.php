6ceff84d40a8d06146456e69f8b9ac712fd705bc
jpdomenden <103397922+jpdomenden@users.noreply.github.com>
2022-08-16 09:34:10 +1200

    Add files via upload

--- multiplicationTable.php
+++ multiplicationTable.php
@@ -21,98 +21,25 @@
 </style>
 </head>
 <body>
- <h1 style="font-size:50px">Multiplication Table</h1>
- <div class="flex-container">
-    <div style="color:blue">    <?php
-
-    for($i=1; $i<=10; $i++){
-        echo '1' . '*' . $i . '=' . $i * 1 ;
-        echo "<br>";
-    }?>
-    <br>
+  <div class= "flex-container";>
+    <div>    <?php
+    $num =1; 
+    while($num <=10){
+        for($i=1; $i<=10; $i++){
+            echo $num . '*' . $i . '=' . $i * 1 ;
+            echo "<br>";
+    }
+    ?>
     </div>
-    <div style="color:blue">    <?php
-
-    for($i=1; $i<=10; $i++){
-        echo '2' . '*' . $i . '=' . $i * 2 ;
-        echo "<br>";
-    }?>
-    <br>
+    <div><?php
+    $num++;
+}
+    ?>
     </div>
-    <div style="color:blue">    <?php
+  </div>
+  
 
-    for($i=1; $i<=10; $i++){
-        echo '3' . '*' . $i . '=' . $i * 3 ;
-        echo "<br>";
-    }?>
-    <br>
-    </div>
-    <div style="color:blue">    <?php
-
-    for($i=1; $i<=10; $i++){
-        echo '4' . '*' . $i . '=' . $i * 4 ;
-        echo "<br>";
-    }?>
-    <br>
-    </div>
-    <div style="color:blue">    <?php
-
-    for($i=1; $i<=10; $i++){
-        echo '5' . '*' . $i . '=' . $i * 5 ;
-        echo "<br>";
-    }?>
-    <br>
-    </div>
-    <div style="color:blue">    <?php
-
-    for($i=1; $i<=10; $i++){
-        echo '6' . '*' . $i . '=' . $i * 6 ;
-        echo "<br>";
-    }?>
-    <br>
-    </div>
-    <div style="color:blue">    <?php
-
-    for($i=1; $i<=10; $i++){
-        echo '7' . '*' . $i . '=' . $i * 7 ;
-        echo "<br>";
-    }?>
-    <br>
-    </div>
-    <div style="color:blue">    <?php
-
-    for($i=1; $i<=10; $i++){
-        echo '8' . '*' . $i . '=' . $i * 8 ;
-        echo "<br>";
-    }?>
-    <br>
-    </div>
-    <div style="color:blue">    <?php
-
-    for($i=1; $i<=10; $i++){
-        echo '9' . '*' . $i . '=' . $i * 9 ;
-        echo "<br>";
-    }?>
-    <br>
-    </div>
-    <div style="color:blue">    <?php
-
-    for($i=1; $i<=10; $i++){
-        echo '10' . '*' . $i . '=' . $i * 10 ;
-        echo "<br>";
-    }?>
-    <br>
-
-</div>
-
-    <!-- // $i=1;
-    // while($i<=10){
-    //     echo '5' . 'x' . $i . '=' . $i * 5 ;
-    //     $i++;
-    //     echo "<br>";
-        
-    // }
  
-    ?> -->
+   
 </body>
 </html>
