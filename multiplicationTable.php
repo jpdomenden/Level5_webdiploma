bf06d8b44974272f7a910e3cad396c2bd5746d8a
jpdomenden <103397922+jpdomenden@users.noreply.github.com>
2022-08-16 09:37:43 +1200

    Add files via upload

---
+++ multiplicationTable.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+.flex-container {
+  display: flex;
+  background-color: DodgerBlue;
+}
+
+.flex-container > div {
+  background-color: white;
+  margin: 10px;
+  padding: 60px;
+  font-size: 30px;
+}
+
+</style>
+</head>
+<body>
+<h1>Multiplication Table</h1>
+  <div class= "flex-container";>
+   
+    <div>    <?php
+    $num =1; 
+    while($num <=10){
+        for($i=1; $i<=10; $i++){
+            echo $num . '*' . $i . '=' . $i * 1 ;
+            echo "<br>";
+    }
+    ?>
+    </div>
+    <div><?php
+    $num++;
+}
+    ?>
+    </div>
+  </div>
+  
+
+ 
+   
+</body>
+</html>
